4d5aff1b9e3ac3425e69b97370f5dffd4e088857
aryanbariya-tru <aryan.bariya@connexion-technologies.com>
2025-11-20 16:09:35 +0530

    yml file changed to handle both dev and prod with env variables

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,7 @@ export default function Home() {
       <div>
         <ul className="flex space-x-4 text-xl ">
           <li className=" border-2 rounded-lg p-2 text-black hover:bg-blue-500 hover:text-white bg-gray-100">
-            <Link href={"auth"}>Chain of Responsibility</Link>
-          </li>
-          <li className=" border-2 rounded-lg p-2 text-black hover:bg-blue-500 hover:text-white bg-gray-100">
             <Link href={"factory"}>Factory Mode</Link>
-          </li>
-          <li className="border-2 rounded-lg p-2 text-black hover:bg-blue-500 hover:text-white bg-gray-100">
-            <Link href={"decorator"}>Decorator pattern</Link>
           </li>
           <li className="border-2 rounded-lg p-2 text-black hover:bg-blue-500 hover:text-white bg-gray-100">
             <Link href={"school"}>School</Link>
